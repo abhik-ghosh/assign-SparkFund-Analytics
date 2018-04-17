7e5802b87c2e34d71f1bfbb21931b6f961b93151
Abhik Ghosh <abhik.ghosh5@gmail.com>
2018-04-17 18:14:19 +0000

    updated script file

--- Test1.R
+++ Test1.R
@@ -1,1 +1,7 @@
-print("First Case Study")
+print("*********This is our Test File**********")
+
+companies.dataframe <- read.delim("companies.txt")
+rounds2.dataframe <- read.csv("rounds2.csv")
+
+head(companies.dataframe)
+head(rounds2.dataframe)
