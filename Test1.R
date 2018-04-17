606ad0051e43d41831bc3f6373777516f3f386d2
Abhik Ghosh <abhik.ghosh5@gmail.com>
2018-04-17 19:10:46 +0000

    updated script file

--- Test1.R
+++ Test1.R
@@ -5,3 +5,6 @@ rounds2.dataframe <- read.csv("rounds2.csv")
 
 head(companies.dataframe)
 head(rounds2.dataframe)
+
+unique_company_rounds2 <- unique(rounds2.dataframe$company_permalink)
+unique_company_companies <- unique(companies.dataframe$name)
